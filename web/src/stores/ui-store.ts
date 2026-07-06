de4e761f9e1c13f5778975872388aa380c3ad9a9
ip2a <ooooofish@126.com>
2026-07-06 14:02:21 +0800

    feat(web): manage settings dialog in ui store

--- web/src/stores/ui-store.ts
+++ web/src/stores/ui-store.ts
@@ -2,14 +2,18 @@ import { create } from "zustand"
 
 type UiState = {
   homeHeroCollapsed: boolean
+  settingsDialogOpen: boolean
   selectedAgentId: string | null
   setHomeHeroCollapsed: (collapsed: boolean) => void
+  setSettingsDialogOpen: (open: boolean) => void
   setSelectedAgentId: (agentId: string | null) => void
 }
 
 export const useUiStore = create<UiState>((set) => ({
   homeHeroCollapsed: false,
+  settingsDialogOpen: false,
   selectedAgentId: null,
   setHomeHeroCollapsed: (homeHeroCollapsed) => set({ homeHeroCollapsed }),
+  setSettingsDialogOpen: (settingsDialogOpen) => set({ settingsDialogOpen }),
   setSelectedAgentId: (selectedAgentId) => set({ selectedAgentId }),
 }))
